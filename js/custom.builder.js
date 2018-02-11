89d2bc18eb1f4b9e3842ae09ab76592370a91744
Syed Tauhid Ahmed <stahmed001@gmail.com>
2018-02-11 18:25:26 +0600

    phase two

--- js/custom.builder.js
+++ js/custom.builder.js
@@ -10,6 +10,7 @@ var CustomFroalaEditorBuilder = function(editorId, toolbarButtons, pluginsEnable
     this.pluginsEnabled = pluginsEnabled;
     this.apiUrl = apiUrl;
     this.optionList = {};
+    this.handlebarTemplate = "";
 
     // Define Custon Dropdown
     var defineCustomDropdown = function () {
@@ -23,12 +24,6 @@ var CustomFroalaEditorBuilder = function(editorId, toolbarButtons, pluginsEnable
           options: this.optionList,
           callback: function (cmd, val) {
               insertElementInEditor(this, val);
-          },
-          // Callback on refresh.
-          refresh: function ($btn) {
-          },
-          // Callback on dropdown show.
-          refreshOnShow: function ($btn, $dropdown) {
           }
         });
         this.toolbarButtons.push('custom_dropdown');
@@ -42,19 +37,141 @@ var CustomFroalaEditorBuilder = function(editorId, toolbarButtons, pluginsEnable
     }
     
     // Insert the custom element in Editor
-    var insertElementInEditor = function(editor, id) {
-        editor.html.insert('&lt;bcv:insertOutput id="' + id + '"&gt;' +
-            '&lt;/bcv:insertOutput&gt;');
-        editor.undo.saveStep();
+    var insertElementInEditor = function(editor, type) {
+        var data = getTableDataBasedOnType(type);
+        showModalAndInsertTable(editor, data, type);
+    }
+
+    var showModalAndInsertTable = function(editor, data, type) {
+        var checkboxContainerId = "#table-content-checkbox";
+        var contents = prepareCheckboxContents(data);
+        var checkboxFields = prepareCheckboxFields(contents);
+
+        $(checkboxContainerId).html(checkboxFields);
+
+        prepareAndShowShortbyCheckboxes(checkboxContainerId);
+
+        //show modal
+        var modal = $("#checkboxModal").modal({backdrop: "static", keyboard: false});
+        $("#checkboxModal .modal-title").html(type);
+        var template = this.handlebarTemplate;
+        $('#submit-table-data').on('click', function(){
+            
+            editor.html.insert(template);
+            editor.undo.saveStep();
+            $("#checkboxModal").modal('hide');
+        });
+        modal.show();
+    }
+
+    var prepareCheckboxContents = function(data) {
+        var contents = [];
+        $.each(data, function(key, value){
+            if(data[key].schema) {
+                var fieldsArray = {};
+                fieldsArray["path"] =  key + ".schema.";
+                fieldsArray[key] = [];
+                $.each(data[key].schema, function(fieldsKey, fieldsValue){
+                    fieldsArray[key].push(fieldsKey);
+                });
+                contents.push(fieldsArray)
+            } else {
+                contents.push(key);
+            }
+        });
+
+        return contents;
+    }
+
+    var prepareCheckboxFields = function(contents) {
+        var checkboxFields= [];
+        $.each(contents, function (index, content) {
+            var checkboxField;
+            if(typeof(content) === "object"){
+                var path;
+                $.each(content, function(key, objVal){
+                    console.log(key);
+                    if(key == "path") {
+                        path = objVal;
+                        return;
+                    }
+                    checkboxField = '<div class="checkbox-group"> <span class="title">'+ key + ':' +'</span>';
+                    $.each(objVal,function(index, val){
+                        checkboxField = checkboxField + getCheckboxElem(path + val , val);
+                    });
+                });
+                checkboxField = checkboxField + '</div>';
+            } else {
+                checkboxField = getCheckboxElem(content, content);
+            }
+            checkboxFields.push(checkboxField);
+        });
+
+        return checkboxFields;
+    }
+
+    var prepareAndShowShortbyCheckboxes = function(checkboxContainerId) {
+        var sortBycheckboxContainerID = "#sortby-checkbox-group";
+        $(sortBycheckboxContainerID).html("");
+        $(checkboxContainerId + ' input:checkbox').on('click', function(){
+            var checkboxFields = [];
+            var currentlyChosenValues = [];
+            $(sortBycheckboxContainerID + ' input:checked').each(function(){
+                currentlyChosenValues.push(this.value);
+            });
+            $(checkboxContainerId + ' input:checked').each(function() {
+                var value = this.value;
+                var path = value;
+
+                if(path.indexOf('.') > -1) {
+                    value = path.split(".");
+                    value = value[value.length - 1];
+                }
+                checkboxField = getCheckboxElem(path, value);
+                checkboxFields.push(checkboxField);
+            });
+
+            $(sortBycheckboxContainerID).html(checkboxFields);
+            $.each(currentlyChosenValues, function(index, val){
+                $(sortBycheckboxContainerID + ' input[value="'+ val +'"]').prop('checked', true);
+            });
+        });
+    }
+
+    var getCheckboxElem = function(val, label) {
+        return '<div class="checkbox">' + '<label><input type="checkbox" value="'+ val +'">'+ label +'</label>' + "</div>";
     }
     
     // Initialize the editor.
     var initFroalaEditor = function () {
         defineCustomDropdown();
         $(this.editorId).froalaEditor({
-            toolbarButtons: this.toolbarButtons,
-            pluginsEnabled: this.pluginsEnabled
-        })
+          toolbarButtons: this.toolbarButtons,
+          pluginsEnabled: this.pluginsEnabled,
+          htmlAllowedEmptyTags: [
+            "bcv-output",
+            "bcv-resources"
+          ],
+          htmlAllowedTags: ["bcv-resources"],
+          htmlAllowedAttrs: [".*"],
+          htmlRemoveTags: ["script"]
+        });
+    }
+
+    var getTableDataBasedOnType = function(type) {
+        if(!jsonData)
+            alert("define jsonData. See readme...");
+        return jsonData;
+    }
+
+    var getHandlebarTemplate = function() {
+        $.get( "template.html", function( data ) {
+             setHandlebarTemplate(data);
+        });
+    }
+
+    var setHandlebarTemplate = function(data) {
+        this.handlebarTemplate = data;
     }
     
     // Fetch Custom Buttons Data 
@@ -70,6 +187,7 @@ var CustomFroalaEditorBuilder = function(editorId, toolbarButtons, pluginsEnable
 
     var init = function(){
         getEditorCustomOptions();
+        getHandlebarTemplate();
     }
     
     return {
--- js/main.js
+++ js/main.js
@@ -1,3 +1,77 @@
+//For test Perpose. 
+var jsonData = {
+    "function": {
+      "type": "link",
+      "label": "com.bconview.bcm.bia.CriticalVendorDep.fld.function",
+      "helpText": "com.bconview.bcm.bia.CriticalVendorDep.fld.function.help",
+      "linkLabel": "com.bconview.bcm.bia.CriticalVendorDep.lnk.requiredFor",
+      "resourceType": "com.bconview.bcm.bia.EssentialFunction",
+      "linkScope": "WS_ONLY",
+      "component": "com.bconview.bcm.bia",
+      "required": true
+    },
+    "vendor": {
+      "type": "link",
+      "label": "com.bconview.bcm.bia.CriticalVendorDep.fld.vendor",
+      "helpText": "com.bconview.bcm.bia.CriticalVendorDep.fld.vendor.help",
+      "linkLabel": "critical",
+      "component": "com.bconview.bcm.catalog",
+      "resourceType": "com.bconview.bcm.catalog.Vendor",
+      "linkScope": "WS_AND_HIERARCHY",
+      "allowEventPropagation": "CONTENT_STATUS_CHANGE",
+      "creatable": true,
+      "required": false
+    },
+    "offer": {
+      "type": "text",
+      "label": "com.bconview.bcm.bia.CriticalVendorDep.fld.offer",
+      "helpText": "com.bconview.bcm.bia.CriticalVendorDep.fld.offer.help",
+      "required": true
+    },
+    "contacts": {
+      "type": "embedded_list",
+      "label": "com.bconview.bcm.bia.CriticalVendorDep.fld.contacts",
+      "helpText": "com.bconview.bcm.bia.CriticalVendorDep.fld.contacts.help",
+      "template": "{{ fullName }}",
+      "required": false,
+      "schema": {
+        "fullName": {
+          "type": "text",
+          "label": "com.bconview.bcm.bia.CriticalVendorDep.fld.contacts.fullName",
+          "required": true
+        },
+        "phoneNos": {
+          "type": "text_list",
+          "required": false,
+          "label": "com.bconview.bcm.bia.CriticalVendorDep.fld.contacts.phoneNos",
+          "helpText": "com.bconview.bcm.bia.CriticalVendorDep.fld.contacts.phoneNos.help"
+        },
+        "emails": {
+          "type": "email",
+          "required": false,
+          "label": "com.bconview.bcm.bia.CriticalVendorDep.fld.contacts.email",
+          "helpText": "com.bconview.bcm.bia.CriticalVendorDep.fld.contacts.email.help"
+        }
+      }
+    },
+    "notes": {
+      "type": "text",
+      "label": "com.bconview.bcm.bia.CriticalVendorDep.fld.notes"
+    }
+  };
+
+var customFroalaEditorBuilder;
+//for test purpose Handlebar Template 
+function openPreviewModal() {
+  var html = $('div#froala-editor').froalaEditor('html.get');
+  // html = html.replace(/&lt;/g, "<");
+  // html = html.replace(/&gt;/g, ">");
+  var template = Handlebars.compile(html);
+  html = template(jsonData);
+  console.log(html);
+}
+
 $(function () {
-    CustomFroalaEditorBuilder("#froala-editor", ['bold', 'italic'], [], "/data.json").init();
+    customFroalaEditorBuilder = CustomFroalaEditorBuilder("#froala-editor", ['bold','html'], ['codeView'], "/data.json");
+    customFroalaEditorBuilder.init();
 })
